7e9e35101e9161e9d67749d2c96f1db7d1570ed4
scarlet <scarryaa@gmail.com>
2025-07-14 05:04:28 -0400

    Add directory selection button to title bar

--- lib/shared/widgets/title_bar_widget.dart
+++ lib/shared/widgets/title_bar_widget.dart
@@ -1,11 +1,18 @@
+import 'dart:io';
+
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:rei/features/file_explorer/providers/file.dart';
 
-class TitleBarWidget extends StatelessWidget {
+class TitleBarWidget extends HookConsumerWidget {
   const TitleBarWidget({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final fileExplorerState = ref.watch(fileProvider);
+    final fileNotifier = ref.read(fileProvider.notifier);
+
     return Container(
       decoration: BoxDecoration(
         border: Border(
@@ -13,7 +20,27 @@ class TitleBarWidget extends StatelessWidget {
         ),
       ),
       height: 30.0,
-      child: MoveWindow(),
+      child: MoveWindow(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(width: Platform.isMacOS ? 80.0 : 8.0),
+            TextButton(
+              style: ButtonStyle(
+                minimumSize: WidgetStatePropertyAll(Size(0.0, 0.0)),
+                foregroundColor: WidgetStatePropertyAll(Color((0xAAFFFFFF))),
+              ),
+              onPressed: () => fileNotifier.selectDirectory(),
+              child: Text(
+                fileExplorerState.root?.path
+                        .split(Platform.pathSeparator)
+                        .last ??
+                    'Select a directory',
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
